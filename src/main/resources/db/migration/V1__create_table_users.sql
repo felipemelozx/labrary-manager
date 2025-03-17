34d643e7319e534b9a9bf3c8dc503069ac948b6d
Felipe Melo <felipemelozx@gmail.com>
2025-03-17 15:47:13 -0300

    Remove dependency flywaydb

--- src/main/resources/db/migration/V1__create_table_users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tb_users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(150) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    phone VARCHAR(20),
-    address TEXT,
-    role VARCHAR(20) CHECK (role IN ('USER', 'ADMIN')) DEFAULT 'USER',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
